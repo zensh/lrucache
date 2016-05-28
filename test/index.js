1890be2f6747ac216a487666b3b69b59c804f7a7
zensh <admin@zensh.com>
2016-05-28 12:53:48 +0800

    update devDependencies

--- test/index.js
+++ test/index.js
@@ -1,51 +1,48 @@
 'use strict'
-/* global describe, it */
 
-var should = require('should')
+var tman = require('tman')
+var assert = require('assert')
 var LRUCache = require('../index.js')
 var x = {a: 1, b: 2}
 
-describe('LRUCache', function () {
-  it('LRUCache()', function () {
-    var cache = LRUCache(100)
-    should(cache.info().length).be.equal(0)
-    should(cache.info().capacity).be.equal(100)
-    should(cache.has('a')).be.equal(false)
-    should(cache.get('a')).be.equal(undefined)
-    should().be.equal()
+tman.it('LRUCache', function () {
+  var cache = LRUCache(100)
+  assert.strictEqual(cache.info().length, 0)
+  assert.strictEqual(cache.info().capacity, 100)
+  assert.strictEqual(cache.has('a'), false)
+  assert.strictEqual(cache.get('a'), undefined)
 
-    cache.set('a', x)
-    should(cache.info().length).be.equal(1)
-    should(cache.has('a')).be.equal(true)
-    should(cache.get('a')).be.equal(x)
+  cache.set('a', x)
+  assert.strictEqual(cache.info().length, 1)
+  assert.strictEqual(cache.has('a'), true)
+  assert.strictEqual(cache.get('a'), x)
 
-    cache.set('b', [])
-    should(cache.info().length).be.equal(2)
-    should(cache.get('b')).be.eql([])
+  cache.set('b', [])
+  assert.strictEqual(cache.info().length, 2)
+  assert.deepEqual(cache.get('b'), [])
 
-    cache.set('c', null)
-    should(cache.info().length).be.equal(3)
-    should(cache.get('c')).be.eql(null)
+  cache.set('c', null)
+  assert.strictEqual(cache.info().length, 3)
+  assert.strictEqual(cache.get('c'), null)
 
-    should(cache.keys()).be.eql(['c', 'b', 'a'])
-    cache.get('b')
-    should(cache.keys()).be.eql(['b', 'c', 'a'])
-    cache.remove('c')
-    should(cache.get('c')).be.eql(undefined)
-    should(cache.keys()).be.eql(['b', 'a'])
+  assert.deepEqual(cache.keys(), ['c', 'b', 'a'])
+  cache.get('b')
+  assert.deepEqual(cache.keys(), ['b', 'c', 'a'])
+  cache.remove('c')
+  assert.strictEqual(cache.get('c'), undefined)
+  assert.deepEqual(cache.keys(), ['b', 'a'])
 
-    cache.update('a', function (a) {
-      should(a).be.equal(x)
-      a.a = 2
-      return a
-    })
-    cache.update('c', function (a) {
-      should(true).be.equal(false)
-    })
-
-    should(cache.get('a')).be.eql({a: 2, b: 2})
-    should(cache.keys()).be.eql(['a', 'b'])
-    should(cache.popStale()).be.eql(['b', []])
-    should(cache.keys()).be.eql(['a'])
+  cache.update('a', function (a) {
+    assert.strictEqual(a, x)
+    a.a = 2
+    return a
   })
+  cache.update('c', function (a) {
+    assert.strictEqual(true, false)
+  })
+
+  assert.deepEqual(cache.get('a'), {a: 2, b: 2})
+  assert.deepEqual(cache.keys(), ['a', 'b'])
+  assert.deepEqual(cache.popStale(), ['b', []])
+  assert.deepEqual(cache.keys(), ['a'])
 })
